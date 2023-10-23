0e2174ab97310f9e172aac7a5748e06dbd05be3c
archibird <robindbor@gmail.com>
2023-10-23 12:50:01 -0700

    attempting to format my lua config so it looks exactly like the other plugins, but from a store path. Does not work anymore currently I dont think.

--- default.nix
+++ default.nix
@@ -1,5 +1,16 @@
 { self, pkgs }:
 let
+  #LuaFS = {
+  #  dir1 = {
+  #     subdir1 = {
+  #       file1 = filecontents;
+  #       file2 = filecontents;
+  #     }
+  #     file3 = filecontents;
+  #   }
+  #   dir2 = { file4 = filecontents; };
+  #   file5 = filecontents;
+  # }
   LuaFS = import ./FileSysImport.nix self;
 in
 { url = ""; flake = false; }
