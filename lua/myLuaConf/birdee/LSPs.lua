2caf747fc16a4c77c3bd731702b9356254fed51f
archibird <robindbor@gmail.com>
2023-10-25 05:54:18 -0700

    lsp attempt 2

--- lua/myLuaConf/birdee/LSPs.lua
+++ lua/myLuaConf/birdee/LSPs.lua
@@ -2,29 +2,29 @@ vim.lsp.set_log_level("debug")
 -- require('fidget').setup()
 -- require('neodev').setup({})
 vim.lsp.start({
-  name = "lua-language-server",
-  cmd = { "lua-language-server" },
+  name = "lua_ls",
+  cmd = { "lua-lsp" },
   before_init = require("neodev.lsp").before_init,
   root_dir = vim.fn.getcwd(),
   settings = { Lua = {} },
 })
 
-require'lspconfig'.lua_ls.setup {
-  capabilities = require("myLuaConf.cap-onattach").get_capabilities(),
-  on_attach = require("myLuaConf.cap-onattach").on_attach,
-  filetypes = { "lua" },
-  cmd = { "lua-lsp" },
-  -- settings = {
-  --   Lua = {
-  --     formatters = {
-  --       ignoreComments = true,
-  --     },
-  --     signatureHelp = { enabled = true },
-  --   },
-  --   workspace = { checkThirdParty = true },
-  --   telemetry = { enabled = false },
-  -- },
-}
+-- require'lspconfig'.lua_ls.setup {
+--   capabilities = require("myLuaConf.cap-onattach").get_capabilities(),
+--   on_attach = require("myLuaConf.cap-onattach").on_attach,
+--   filetypes = { "lua" },
+--   cmd = { "lua-lsp" },
+--   -- settings = {
+--   --   Lua = {
+--   --     formatters = {
+--   --       ignoreComments = true,
+--   --     },
+--   --     signatureHelp = { enabled = true },
+--   --   },
+--   --   workspace = { checkThirdParty = true },
+--   --   telemetry = { enabled = false },
+--   -- },
+-- }
 -- require'lspconfig'.nil_ls.setup {
 --   capabilities = require("birdee.lsp.birdeelspconfigs").get_capabilities(),
 --   on_attach = require("birdee.lsp.birdeelspconfigs").on_attach,
