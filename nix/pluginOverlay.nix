3fe03276188c3f4eebd8bfff54e824b790f362a6
archibird <robindbor@gmail.com>
2023-11-02 13:17:29 -0700

    self super instead of final prev for convention's sake

--- nix/pluginOverlay.nix
+++ nix/pluginOverlay.nix
@@ -1,9 +1,9 @@
 inputs:
   # Once we add this overlay to our nixpkgs, we are able to
   # use `pkgs.neovimPlugins`, which is a map of our plugins.
-(final: prev:
+(self: super:
 let
-  inherit (prev.vimUtils) buildVimPlugin;
+  inherit (super.vimUtils) buildVimPlugin;
   plugins = builtins.filter
     (s: (builtins.match "plugins-.*" s) != null)
     (builtins.attrNames inputs);
