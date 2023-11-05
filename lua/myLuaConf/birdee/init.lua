97463a8d9842fbce218c8a1583230b3a47de763d
archibird <robindbor@gmail.com>
2023-11-04 23:42:56 -0700

    can now easily change the folder required by init.vim

--- lua/myLuaConf/birdee/init.lua
+++ lua/myLuaConf/birdee/init.lua
@@ -1,7 +1,5 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
--- I just passed categories into everything 
--- whether it was needed or not
 require("myLuaConf.birdee.plugins")
 require("myLuaConf.birdee.LSPs")
 require("myLuaConf.birdee.debug")
