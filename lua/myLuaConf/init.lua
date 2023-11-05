aa1c7998fc4ddd5fdd53086978a9e5fd5c375990
archibird <robindbor@gmail.com>
2023-11-04 22:26:38 -0700

    Now with more cats and less pain

--- lua/myLuaConf/init.lua
+++ lua/myLuaConf/init.lua
@@ -1,24 +1,30 @@
-  -- if you have your own lua config, it starts here.
-  -- when you want to query if a category is enabled do:
-  -- local categories = require('nixCats')
-  -- if(categories.categoryname) then
+--[[ Lua config Intro: meet nixCats!
 
-  -- It contains the table of name = boolean values 
-  -- you created when choosing categories of packages and plugins
-  -- nothing happens if you dont pass it in but you wont be able to use it.
-  -- I just passed it in everywhere because why not.
+if you have your own lua config, it starts here.
+when you want to query if a category is enabled do:
 
-  -- use lspconfig and dap and setup functions. 
-  -- no package manager or mason
-  -- (they honestly might still work though I dont actually know)
-  -- (It would kinda defeat the purpose though)
+local categories = require('nixCats')
+if(categories.categoryname) then
+  do stuff
+end
 
-  -- other than that, its just a normal config now.
+It contains the table of name = boolean values 
+you created when choosing categories of packages and plugins
+
+use lspconfig and dap and setup functions. 
+no package manager or mason
+(they honestly might still work though I dont actually know)
+(It would kinda defeat the purpose though)
+
+other than that, its just a normal config now. 
+]]
+
 if vim.g.vscode == nil then
+  -- I made nixCats recently so I still just pass it around everywhere
+  -- will check it where relevant instead soon rather than here
   local categories = require('nixCats')
   require("myLuaConf.birdee").setup(categories)
 else
-  -- just in case I need to show someone something in vscode idk
   -- currently not implemented
   -- its basically to be just a stripped down version for embedding
   require('myLuaConf.vscody')
