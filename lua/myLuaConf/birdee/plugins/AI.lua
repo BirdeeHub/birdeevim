2f23bd438b7ecddd318141eb37b4c82d0e1b8220
BirdeeHub <robindbor@gmail.com>
2023-11-22 10:33:20 -0800

    auth stuff

--- lua/myLuaConf/birdee/plugins/AI.lua
+++ lua/myLuaConf/birdee/plugins/AI.lua
@@ -3,20 +3,44 @@ require("sg").setup({
   enable_cody = true,
 })
 local result
-local appname = vim.fn.expand('$NVIM_APPNAME')
-if (appname == "$NVIM_APPNAME") then
-  appname = 'nvim'
+local tokenPath = vim.fn.expand("$HOME") .. "/.secrets/codyToken"
+if vim.fn.filereadable(tokenPath) == 1 then
+  local handle = io.open(tokenPath, "r")
+  if handle then
+    result = handle:read("*l")
+    handle:close()
+  end
+  require('sg.auth').set_nvim_auth({
+    tos_accepted = true,
+    endpoint = 'https://sourcegraph.com',
+    token = result,
+  })
 end
-local handle = io.popen("cat ~/.cache/".. appname .."/codyToken")
-if handle then
-  result = handle:read("*a")
-  handle:close()
-end
-require('sg.auth').set_nvim_auth({
-  tos_accepted = true,
-  endpoint = 'https://sourcegraph.com',
-  token = result,
-})
 vim.keymap.set('n', '<leader>cs', require('sg.extensions.telescope').fuzzy_search_results, { noremap = true, desc = 'cody search' })
 vim.keymap.set('n', '<leader>cc', [[<cmd>CodyToggle<CR>]], { noremap = true, desc = 'CodyChat' })
 vim.keymap.set('v', '<leader>cc', [[:CodyAsk ]], { noremap = true, desc = 'CodyAsk' })
+
+local full_dir_path = vim.fn.stdpath('cache') .. '/' .. 'codeium'
+local full_file_path = full_dir_path .. '/' .. 'config.json'
+local keyPath = vim.fn.expand("$HOME") .. "/.secrets/codeiumToken"
+if vim.fn.filereadable(keyPath) == 1 then
+  if vim.fn.filereadable(full_file_path) == 0 then
+    local codeiumKey
+    local codeiumHandle = io.open(keyPath, "r")
+    if codeiumHandle then
+      codeiumKey = codeiumHandle:read("*l")
+      codeiumHandle:close()
+    end
+    if vim.fn.isdirectory(full_dir_path) == 0 then
+      -- Directory does not exist, so create it
+      vim.fn.mkdir(full_dir_path, 'p')
+    end
+    -- Open the file in write mode
+    local file = io.open(full_file_path, 'w')
+    -- Check if the file was successfully opened
+    if file then
+      file:write('{"api_key": "' .. codeiumKey .. '"}')
+      file:close()
+    end
+  end
+end
