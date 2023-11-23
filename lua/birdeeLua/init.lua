265b8c4a9dff25e4dcb3bb7951dfbc91abf523c4
BirdeeHub <robindbor@gmail.com>
2023-11-23 11:47:50 -0800

    unnecessary changes for dynamicness

--- lua/birdeeLua/init.lua
+++ lua/birdeeLua/init.lua
@@ -1,26 +1,3 @@
---[[ Lua config Intro: meet nixCats!
-
-if you have your own lua config, it starts here.
-Or you can keep this config around without deleting
-but use a different one by changing RCName in the flake.nix file.
-
-when you want to query if a category is enabled do:
-
-local categories = require('nixCats')
-if(categories.categoryname) then
-  do stuff
-end
-
-It contains the table of name = boolean values 
-you created when choosing categories of packages and plugins
-
-use lspconfig and dap and setup functions. 
-no package manager or mason
-(they honestly might still work though I dont actually know)
-(It would kinda defeat the purpose though)
-
-other than that, its just a normal config now. 
-]]
 
 if vim.g.vscode == nil then
   require(require('nixCats').RCName .. ".birdee")
