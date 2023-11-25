253a7778d82edbfb7ed16d35bb68a3cf8b62e0a6
BirdeeHub <robindbor@gmail.com>
2023-11-25 02:56:01 -0800

    comment

--- lua/birdeeLua/init.lua
+++ lua/birdeeLua/init.lua
@@ -2,7 +2,6 @@
 if vim.g.vscode == nil then
   require(require('nixCats').RCName .. ".birdee")
 else
-  -- currently not implemented
-  -- its basically to be just a stripped down version for embedding
+  -- a stripped down version for embedding
   require(require('nixCats').RCName .. '.vscody')
 end
