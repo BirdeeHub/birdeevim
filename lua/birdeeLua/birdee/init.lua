c99a693cf638925ac8411f502449b4649a6f7e41
BirdeeHub <robindbor@gmail.com>
2023-11-25 02:52:16 -0800

    category checks

--- lua/birdeeLua/birdee/init.lua
+++ lua/birdeeLua/birdee/init.lua
@@ -2,7 +2,9 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 require(require('nixCats').RCName .. ".birdee.plugins")
 require(require('nixCats').RCName .. ".birdee.LSPs")
-require(require('nixCats').RCName .. ".birdee.debug")
+if require('nixCats').debug then
+  require(require('nixCats').RCName .. ".birdee.debug")
+end
 require(require('nixCats').RCName .. ".birdee.format")
 require(require('nixCats').RCName .. ".birdee.keymaps")
 require(require('nixCats').RCName .. ".clippy")
