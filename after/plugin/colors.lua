d578a3d6176893bf68793518eb43749d4e334400
BirdeeHub <robindbor@gmail.com>
2023-12-14 23:53:47 -0800

    nvim

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,1 +1,3 @@
-vim.cmd([[hi LineNr guifg=#bb9af7]])
+if require('nixCats').nixCats_packageName ~= "minimal" then
+  vim.cmd([[hi LineNr guifg=#bb9af7]])
+end
--- init.lua
+++ init.lua
@@ -1,7 +1,9 @@
 -- print(debug.getinfo(1, "S").source:sub(2))
-if vim.g.vscode == nil then
-  require("birdee")
-else
-  -- a stripped down version for embedding
-  require('vscody')
+if require('nixCats').nixCats_packageName ~= "minimal" then
+  if vim.g.vscode == nil then
+    require("birdee")
+  else
+    -- a stripped down version for embedding
+    require('vscody')
+  end
 end
