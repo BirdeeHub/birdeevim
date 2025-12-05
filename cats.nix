c527a19a2884d08b6513717186c550d65c47c2ec
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2025-12-05 05:26:28 -0800

    Added a dance routine to the file upload function
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   cats.nix
    	modified:   flake.lock

--- cats.nix
+++ cats.nix
@@ -229,7 +229,7 @@ in { pkgs, settings, categories, name, extra, mkPlugin, ... }@packageDef: {
       scooter = [
         (inputs.wrappers.lib.wrapPackage {
           inherit pkgs;
-          package = inputs.scooter.packages.${pkgs.stdenv.hostPlatform.system}.default.overrideAttrs { meta.mainProgram = "scooter"; }; # override temporary to remove warning until PR happens
+          package = inputs.scooter.packages.${pkgs.stdenv.hostPlatform.system}.default;
           flags."--config-dir" = "${placeholder "out"}/share/bundled_config";
           drv.passAsFile = [ "scootcfg" ];
           drv.scootcfg = /*toml*/''
