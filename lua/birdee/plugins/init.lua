c84dd9f717961945f8183570e2096b34ea33ddea
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-01-20 14:13:20 -0800

    Added a cheese wedge to the server room for debugging purposes
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   lua/birdee/plugins/init.lua

--- lua/birdee/plugins/init.lua
+++ lua/birdee/plugins/init.lua
@@ -21,7 +21,6 @@ if nixInfo.utils.get_nix_plugin_path "argmark" then
   end, { desc = "Arglist local/global toggle" })
 end
 
--- TODO: make an option to disable or enable this, it isnt a plugin
 if nixInfo(false, "info", "cats", "scooter") then
   vim.keymap.set('n', '<leader>rr', function() require('scooter').open_scooter() end, { desc = 'Open scooter' })
   vim.keymap.set('v', '<leader>rr',
