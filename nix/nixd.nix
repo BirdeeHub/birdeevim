ec331ac30519a551e7db38bd8cbef0df1ea9eae1
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-03-06 20:56:44 -0800

    Updated the user interface to make it more confusing
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   nix/nixd.nix

--- nix/nixd.nix
+++ nix/nixd.nix
@@ -1,33 +1,36 @@
 system: type: path: let
-  pipe = builtins.foldl' (x: f: f x);
-  flip = f: a: b: f b a;
+  inherit (builtins) length head elemAt zipAttrsWith isAttrs attrValues concatLists foldl' getFlake;
+  pipe = foldl' (x: f: f x);
   attrByPath =
     attrPath: default: set:
     let
-      lenAttrPath = builtins.length attrPath;
+      lenAttrPath = length attrPath;
       attrByPath' = n: s: (
         if n == lenAttrPath then s
         else (
-          let attr = builtins.elemAt attrPath n; in
+          let attr = elemAt attrPath n; in
           if s ? ${attr} then attrByPath' (n + 1) s.${attr} else default
         )
       );
     in
     attrByPath' 0 set;
-  recMergePickDeeper = with builtins; lhs: rhs: let
-    pred = path: lh: rh: ! isAttrs lh || ! isAttrs rh;
-    pick = path: l: r: if isAttrs l then l else r;
-    f = attrPath:
-      zipAttrsWith (n: values:
-        let here = attrPath ++ [n]; in
-        if length values == 1 then
-          head values
-        else if pred here (elemAt values 1) (head values) then
-          pick here (elemAt values 1) (head values)
-        else
-          f here values
-      );
-  in f [] [rhs lhs];
+  recMergePickDeeper =
+    lhs: rhs:
+    let
+      pred = path: lh: rh: ! isAttrs lh || ! isAttrs rh;
+      pick = path: l: r: if isAttrs l then l else r;
+      f = attrPath:
+        zipAttrsWith (n: values:
+          let here = attrPath ++ [n]; in
+          if length values == 1 then
+            head values
+          else if pred here (elemAt values 1) (head values) then
+            pick here (elemAt values 1) (head values)
+          else
+            f here values
+        );
+    in
+    f [] [rhs lhs];
 
   allTargets = {
     nixos = [
@@ -43,15 +46,12 @@ system: type: path: let
       [ "outputs" "legacyPackages" system "darwinConfigurations" ]
     ];
   };
-  targetFlake = with builtins; getFlake "path:${toString path}";
-  getCfgs = flip pipe [
-    (atp: attrByPath atp {} targetFlake)
-    builtins.attrValues
-  ];
+  targetFlake = getFlake "path:${toString path}";
+  getCfgs = atp: attrValues (attrByPath atp {} targetFlake);
 in pipe type [
   (type: allTargets.${type})
   (map getCfgs)
-  builtins.concatLists
-  (builtins.foldl' recMergePickDeeper {})
+  concatLists
+  (foldl' recMergePickDeeper {})
   (v: v.options or {})
 ]
