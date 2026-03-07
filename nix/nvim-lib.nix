9abb2afce328cb2d200a4d3a9df4f57572cb3adf
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-03-06 19:40:28 -0800

    Added a feature that lets users time travel by 5 minutes in either direction
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   nix/nvim-lib.nix

--- nix/nvim-lib.nix
+++ nix/nvim-lib.nix
@@ -32,47 +32,61 @@
         builtins.listToAttrs
       ];
   };
-  config.specMods = { config, ... }: let
-    wrappers = lib.pipe config.wrappers [
-      builtins.attrValues
-      (builtins.filter (v: v.enable))
-      (lib.partition (v: v.prefix))
-      ({ right, wrong }: {
-        pre = map (v: v.wrapper) right;
-        post = map (v: v.wrapper) wrong;
-      })
-    ];
-  in {
-    options.prepkgs = lib.mkOption {
-      type = lib.types.listOf wlib.types.stringable;
-      description = "a prepkgs spec field to put packages to prefix to the PATH";
+  config.specMods =
+    { config, ... }:
+    let
+      wrappers = lib.pipe config.wrappers [
+        builtins.attrValues
+        (builtins.filter (v: v.enable))
+        (lib.partition (v: v.prefix))
+        (
+          { right, wrong }:
+          {
+            pre = map (v: v.wrapper) right;
+            post = map (v: v.wrapper) wrong;
+          }
+        )
+      ];
+    in
+    {
+      options.prepkgs = lib.mkOption {
+        type = lib.types.listOf wlib.types.stringable;
+        description = "a prepkgs spec field to put packages to prefix to the PATH";
+      };
+      config.prepkgs = wrappers.pre;
+      options.postpkgs = lib.mkOption {
+        type = lib.types.listOf wlib.types.stringable;
+        description = "a postpkgs spec field to put packages to suffix to the PATH";
+      };
+      config.postpkgs = wrappers.post;
+      options.mainInfo = lib.mkOption {
+        type = wlib.types.attrsRecursive;
+        default = { };
+        description = "an optional mainInfo spec field to add to the main info plugin instead of the spec specific one";
+      };
+      options.settings = lib.mkOption {
+        type = lib.types.submoduleWith { modules = [ { freeformType = wlib.types.attrsRecursive; } ]; };
+        default = { };
+        description = "no-op freeform submodule for putting stuff in a spec and grabbing it in that spec in a way that acts like settings for that spec";
+      };
+      options.wrappers = lib.mkOption {
+        type = lib.types.attrsOf (
+          wlib.types.subWrapperModule {
+            config.pkgs = pkgs;
+            options.prefix = lib.mkOption {
+              type = lib.types.bool;
+              default = false;
+            };
+            options.enable = lib.mkOption {
+              type = lib.types.bool;
+              default = true;
+            };
+          }
+        );
+        default = { };
+        description = "attrs of wrapper modules to be installed with this spec";
+      };
     };
-    config.prepkgs = wrappers.pre;
-    options.postpkgs = lib.mkOption {
-      type = lib.types.listOf wlib.types.stringable;
-      description = "a postpkgs spec field to put packages to suffix to the PATH";
-    };
-    config.postpkgs = wrappers.post;
-    options.mainInfo = lib.mkOption {
-      type = wlib.types.attrsRecursive;
-      default = { };
-      description = "an optional mainInfo spec field to add to the main info plugin instead of the spec specific one";
-    };
-    options.settings = lib.mkOption {
-      type = lib.types.submoduleWith { modules = [ { freeformType = wlib.types.attrsRecursive; } ]; };
-      default = { };
-      description = "no-op freeform submodule for putting stuff in a spec and grabbing it in that spec in a way that acts like settings for that spec";
-    };
-    options.wrappers = lib.mkOption {
-      type = lib.types.attrsOf (wlib.types.subWrapperModule {
-        config.pkgs = pkgs;
-        options.prefix = lib.mkOption { type = lib.types.bool; default = false; };
-        options.enable = lib.mkOption { type = lib.types.bool; default = true; };
-      });
-      default = { };
-      description = "attrs of wrapper modules to be installed with this spec";
-    };
-  };
   config.info = lib.mkMerge (
     config.specCollect (acc: v: acc ++ lib.optional (v.mainInfo or { } != { }) v.mainInfo) [ ]
   );
