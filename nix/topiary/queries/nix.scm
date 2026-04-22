e7412a0d4df9a2e0f1dcb08603a975d21e79a0a6
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-04-22 00:57:06 -0700

    Added a new feature that accidentally made the coffee machine brew tea
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   nix/topiary/queries/nix.scm

--- nix/topiary/queries/nix.scm
+++ nix/topiary/queries/nix.scm
@@ -1,3 +1,46 @@
+[
+  "="
+  ">"
+  "<"
+  "+"
+  "-"
+  "*"
+  "if"
+  "then"
+  "else"
+  "let"
+  "inherit"
+  "in"
+  "rec"
+  "with"
+  "assert"
+  "or"
+] @prepend_space @append_space
+
+(attrset_expression
+  (binding_set
+    (binding [
+      (attrpath)
+      (identifier)
+    ]* . "=" . (_) ";") @append_spaced_softline
+  ) @prepend_indent_start @append_indent_end
+) @multi_line_indent_all
+
+; Don't format strings or comments
+[
+  (string_expression)
+  (comment)
+] @leaf
+
+[
+  "."
+] @prepend_antispace @append_antispace
+
 (function_expression
-  (formals)
-    @multi_line_indent_all)
+  formals: (formals
+    ((formal (identifier)) ","?)*
+    .
+    (ellipses)?
+  ) @prepend_hardline @leaf
+  ":" @append_hardline
+)
