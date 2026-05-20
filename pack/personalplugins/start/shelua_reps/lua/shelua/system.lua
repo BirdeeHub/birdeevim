991f275516333ea4df81a74fc5918d813c81a71a
Birdee <85372418+BirdeeHub@users.noreply.github.com>
2026-05-19 20:12:06 -0700

    Fixed the flux capacitor again
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   pack/personalplugins/start/shelua_reps/lua/shelua/system.lua

--- pack/personalplugins/start/shelua_reps/lua/shelua/system.lua
+++ pack/personalplugins/start/shelua_reps/lua/shelua/system.lua
@@ -125,7 +125,7 @@ local MAX_TIMEOUT = 2 ^ 31 - 1
 function SystemObj:wait(timeout)
   local state = self._state
 
-  local done = (vim.wait or wait_loop)(timeout or state.timeout or MAX_TIMEOUT, function()
+  local done = ((vim or {}).wait or wait_loop)(timeout or state.timeout or MAX_TIMEOUT, function()
     return state.result ~= nil
   end, nil, true)
 
